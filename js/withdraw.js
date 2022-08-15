af2ade7a76f84a57c9cb20081ea4f479b9d848ab
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-16 00:18:44 +0200

    Validation added

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,6 +7,14 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
     const withdrawInputValueString  = withdrawInputField.value;
     const withdrawInputValue = parseFloat(withdrawInputValueString);
 
+    // ** NaN check
+
+    if (isNaN( withdrawInputValue)) {
+        alert('Please enter a valid value');
+        withdrawInputField.value = '';
+        return;
+    }
+
     // ** Step-3 Now we'll grab the previous withdraw total 
 
     const previousWithdrawTotalField = document.getElementById('withdraw-total');
@@ -17,23 +25,37 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
 
     const totalWithdraw = withdrawInputValue + previousWithdrawTotalValue;
 
-    // ** Step-5 Update the previous withdraw total field
 
-    previousWithdrawTotalField.innerText = totalWithdraw;
 
     // ** Step-6 Now we'll grab the previous balance field 
 
     const previousBalanceField = document.getElementById('balance-display');
     const previousBalanceValueString = previousBalanceField.innerText;
     const previousBalanceValue = parseFloat(previousBalanceValueString);
+
+     // ** Validation 
+
+     if (withdrawInputValue > previousBalanceValue ) {
+        alert('Not Enough Money available');
+        withdrawInputField.value = '';
+        return;
+    }
+    
+    // ** Step-5 Update the previous withdraw total field
+
+    previousWithdrawTotalField.innerText = totalWithdraw;
+
     
     // ** Step-7 We'll set the new balance after withdraw
 
     const totalBalanceAfterWithdraw = previousBalanceValue - withdrawInputValue;
 
+
     // ** Step-8 We' set the new balance after withdraw
 
     previousBalanceField.innerText = totalBalanceAfterWithdraw;
+
+    
 
     // ** last step -> clear the input field
 
