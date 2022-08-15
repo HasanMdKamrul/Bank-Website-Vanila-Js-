96ff59bef47cfeaa2cedbbdc2f9ce12ca57be2bd
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-15 21:38:05 +0200

    How to navigate to another page when certain operation is done

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ document.getElementById('submit-button').addEventListener('click', function(even
     // Button a  click hole amra email and password field theke tader value niye nibo
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-
-    email === 'baap@bank.com' && password === 'secret' ? console.log('Valid User') : console.log('Invalid User')
-
+    // ** Never do this in production
+    if (email === 'baap@bank.com' && password === 'secret') {
+        window.location.href = 'http://127.0.0.1:5500/bank.html' 
+    } else {
+        alert('Invalid User oe Enter valid credentials')
+    }
+   
 })
